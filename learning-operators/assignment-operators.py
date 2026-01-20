2e6d0fc1ce248f7aff1c3ca4b4476ea3edbee49a
sharadkulkarni1997 <sharadkulkarnidb11@gmail.com>
2026-01-20 18:34:28 -0400

    Types of operators

---
+++ learning-operators/assignment-operators.py
@@ -0,0 +1,20 @@
+#assignment operators
+a = 10
+b = 5
+c = 0
+c += a  # c = c + a
+print("After c += a, c =", c)
+c -= b  # c = c - b
+print("After c -= b, c =", c)
+c *= a  # c = c * a
+print("After c *= a, c =", c)
+c /= b  # c = c / b
+print("After c /= b, c =", c)
+c %= a  # c = c % a
+print("After c %= a, c =", c)
+c **= 2  # c = c ** 2 "to the power of"
+print("After c **= 2, c =", c)
+c //= 3  # c = c // 3
+print("After c //= 3, c =", c)
+
+#END
