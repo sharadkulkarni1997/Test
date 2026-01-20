db3c5290087afbf9683a52971ba339a0dfbef29b
sharadkulkarni1997 <sharadkulkarnidb11@gmail.com>
2026-01-20 19:08:38 -0400

    Taking input from user

---
+++ input-in-python/first-program.py
@@ -0,0 +1,18 @@
+#input a string from the user and print it
+name = 0
+print("Hello")
+print("My name is Sharad")
+print('What is your name?')
+name = input()
+print("Hello " + name + "! Nice to meet you.")
+print("Welcome to the world of Python Programming!")
+print("The type of variable entered is: ", type(name))
+# input() by default is a string
+
+#input a int/fload
+val = 0
+print("Enter your age:")
+val = int(input())  #type casting to int
+print("You have entered age as: " , val)
+print("The type of variable entered is: ", type(val))   
+#END
